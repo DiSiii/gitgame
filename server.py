f4b8460d76513c5dc4d36b64860e4e4f10539143
DiSiii <137560373+DiSiii@users.noreply.github.com>
2026-02-07 18:17:03 +0300

    Update server.py

--- server.py
+++ server.py
@@ -120,4 +120,5 @@ def clear_game():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
